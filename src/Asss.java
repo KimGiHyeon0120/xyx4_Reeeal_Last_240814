c839f93cbc41df4c620addf06f2fbd657f549bea
KGH <subinkhd5@naver.com>
2024-08-14 16:56:42 +0900

    6666666666666

--- src/Asss.java
+++ src/Asss.java
@@ -2,5 +2,6 @@
 public class Asss {
 public static void main(String[] args) {
 	System.out.println("ssssssssss");
+	System.out.println("바꾸기1");
 }
 }
--- src/Rasd.java
+++ src/Rasd.java
@@ -3,6 +3,7 @@ public class Rasd {
 
 	public static void main(String[] args) {
 		System.out.println("dddddddd");
+		System.out.println("바꾸기1");
 	}
 
 }
